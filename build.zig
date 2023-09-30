875e9fe6af6fd4bb3db19e3c5b4369c164c8a451
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-09-30 13:37:27 +0300

    change module name

--- build.zig
+++ build.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zesult", .{
-        .source_file = .{ .path = "zesult.zig" },
+    _ = b.addModule("maybe", .{
+        .source_file = .{ .path = "maybe.zig" },
         .dependencies = &[_]std.Build.ModuleDependency{},
     });
 }
