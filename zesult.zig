42294cdd3f6d292a5977a1f0fb7dbba21620526a
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-09-30 08:04:45 +0300

    add Option

---
+++ zesult.zig
@@ -0,0 +1,56 @@
+pub fn Option(comptime T: type) type {
+    return union(enum) {
+        none,
+        some: T,
+
+        const Self = @This();
+
+        pub inline fn None() Self {
+            return .{ .none = {}}; 
+        }
+
+        pub inline fn Some(value: T) Self {
+            return .{ .some = value}; 
+        }
+
+        pub fn isSome(self: Self) bool {
+            switch(self) {
+                .some => return true,
+                else => return false
+            }
+        }
+
+        pub fn isSomeAnd(self: Self, f: *const fn(T) bool) bool {
+            switch(self) {
+                .some => |v| return f(v),
+                else => return false
+            }
+        }
+        
+    };
+}
+
+test "isSome" {
+    const a: Option(u32) = .{ .some = 2};
+    try @import("std").testing.expectEqual(a.isSome(), true);
+    
+    const b: Option(u32) = .{ .none = {}};
+    try @import("std").testing.expectEqual(b.isSome(), false);
+}
+
+test "isSomeAnd" {
+    const function = struct {
+        fn predicate(x: u32) bool {
+            return x > 1;
+        }
+    };
+    
+    const a: Option(u32) = .{ .some = 2};
+    try @import("std").testing.expectEqual(a.isSomeAnd(function.predicate), true);
+    
+    const b: Option(u32) = .{ .some = 0};
+    try @import("std").testing.expectEqual(b.isSomeAnd(function.predicate), false);
+
+    const c: Option(u32) = .{ .none = {}};
+    try @import("std").testing.expectEqual(c.isSomeAnd(function.predicate), false);
+}
