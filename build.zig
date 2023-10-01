6b267e376cdc4dfc1d2e2719809289e5da8e0daf
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-10-01 17:45:12 +0300

    update module path

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("maybe", .{
-        .source_file = .{ .path = "maybe.zig" },
+        .source_file = .{ .path = "src/maybe.zig" },
         .dependencies = &[_]std.Build.ModuleDependency{},
     });
 }
